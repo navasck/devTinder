75edbfa17db33850d140e574b0086bb6d699f208
ABDUL NAVAS CK <cknavas123@gmail.com>
2024-09-21 21:33:28 +0530

    db connected and sample post api created and user model created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
+// const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+// app.use(cookieParser());
+
+const User = require('./models/user');
 
 app.use('/test', (req, res) => {
   res.send('Hello, world!');
@@ -8,6 +14,24 @@ app.use('/test', (req, res) => {
 
 app.use('/hello', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.post('/signup', async (req, res) => {
+
+  // creating a new instance of the User Model
+  const user = new User(req.body);
+  // const user = new User({
+  //   firstName: 'Izan mt k',
+  //   lastName: 'Ck',
+  //   emailId: 'izan123@gmail.com',
+  //   password: 'izan@123',
+  // });
+  try {
+    await user.save();
+    res.send('User Added Succesfully');
+  } catch (err) {
+    res.status(400).send('Error saving the user');
+  }
 });
 
 connectDB()
@@ -20,3 +44,7 @@ connectDB()
   .catch((err) => {
     console.error('Database cannot be connected!!');
   });
+
+// app.listen(7777, () => {
+//   console.log(`Server listening on port 7777...`);
+// });
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   await mongoose.connect(
-    'mongodb+srv://ckndreams:b7kw5DJNO0PyvrqJ@namastedev.aerfr.mongodb.net'
+    'mongodb+srv://ckndreams:b7kw5DJNO0PyvrqJ@namastedev.aerfr.mongodb.net/devTinder'
   );
 };
 
