dbe57d44eef6920a6bd80dad5517e9d509b2fee8
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-01-28 17:32:01 +0530

    rate limit and helmet added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 app.use(
   cors({
@@ -10,8 +14,23 @@ app.use(
     credentials: true,
   })
 );
+
+// Use Helmet middleware
+// app.use(helmet()) - and Helmet will automatically set a variety of security-enhancing headers. These headers help protect against common web vulnerabilities like Cross-Site Scripting (XSS), clickjacking, and more.
+
+app.use(helmet());
+
+// Rate limiting
+// rateLimit(): This function creates a new instance of the rateLimit middleware.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 const authRouter = require('./routes/auth');
 const profileRouter = require('./routes/profile');
